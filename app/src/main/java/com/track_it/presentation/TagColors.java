22c42fe6bc16d721987c3be7473fcf1c64b88ef2
erichseb <erichseb@myumanitoba.ca>
2023-07-25 21:25:27 -0500

    - I added the ability for users to add existing tags being seeing a menu of existing tags, and checking which ones to include.

--- app/src/main/java/com/track_it/presentation/TagColors.java
+++ app/src/main/java/com/track_it/presentation/TagColors.java
@@ -9,34 +9,37 @@ import android.widget.EditText;
 import com.track_it.R;
 import com.track_it.application.SetupParameters;
 
+import afu.org.checkerframework.checker.oigj.qual.O;
 
+
+// This class is used to automatically set the text entered by a user into different colors.
 public class TagColors {
 
 
     private static String[] tagColorList = null; // List of colors (tag colors will iterate through this list)
 
-
     //Set the text watch, such that the colors automatically changes as user types input.
     public static void setTextWatcher(Context context, EditText tagInput) {
         tagInput.addTextChangedListener(new TextWatcher() {
 
+
+            @Override
             public void afterTextChanged(Editable s) {
                 tagInput.removeTextChangedListener(this);
                 int cursorPosition = tagInput.getSelectionStart(); //save cursor position
-
                 TagColors.setTagColors(context, tagInput, tagInput.getText().toString()); //Set colors
-
                 tagInput.setSelection(Math.min(tagInput.getText().length(), cursorPosition)); // Put the cursor back into correct position
                 tagInput.addTextChangedListener(this);
             }
 
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-            }
 
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            //You have to have this dead code here, as abstract methods onTextChanged and afterTextChanged must be defined
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {}
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after){}
 
 
-            }
         });
     }
 
@@ -49,29 +52,29 @@ public class TagColors {
             initColorList(context);
         }
 
-        String[] getSplit = input.split(SetupParameters.getTagHandler().getSplitCriteria()); // Split up the input string into tags
+        String[] getSplit = input.split(SetupParameters.getTagHandler().getSplitCriteria()); // Split up the input string into tags the
 
 
-        int itr = 0;
         String allTagsName = ""; // what we will set the tagInput text to be
-        //Go through each tag and give them a different color
+        //Go through each tag, add them to allTagsName and give them a different color
+        int index = 0;
         for (String currTagName : getSplit) {
 
-            if (!currTagName.trim().equals("")) //Only add word if it is not blank
+            if ( ! currTagName.trim().equals("")) // Only add the word if it is not blank
             {
-                allTagsName += "<span style='background:" + tagColorList[itr % tagColorList.length] + "';>" + currTagName + "</span>";
-                itr++;
-                if (itr < getSplit.length) {
+                allTagsName += "<span style='background:" + tagColorList[index % tagColorList.length] + "';>" + currTagName + "</span>"; // Each tag has a different color
+                if (index + 1 < getSplit.length)
+                {
                     allTagsName += " "; // Add a space between all words, except the last
                 }
             }
+            index++;
         }
 
 
-        //If the last chars of the original string were blank spaces before, add them back now
+        //If the last characterss of the original string were blank spaces before, add them back now
         int endSpaceItr = input.length() - 1;
-        while (endSpaceItr >= 0 && input.charAt(endSpaceItr) == ' ')
-        {
+        while (endSpaceItr >= 0 && input.charAt(endSpaceItr) == ' ') {
             allTagsName += " ";
             endSpaceItr--;
         }
